c0b03213698e938ae115c750a6f71cdd99bf45a7
Mauricio Camacho <mcamacho9729@gmail.com>
2022-11-13 19:56:31 -0600

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
+    case "increase":
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+    case "toggle":
+      return {
+        showCounter: !state.showCounter,
+        counter: state.counter,
+      };
+    default:
+      return {
+        counter: state.counter,
+        showCounter: state.showCounter,
+      };
+  }
+};
+const store = createStore(counterReducer);
+
+export default store;
